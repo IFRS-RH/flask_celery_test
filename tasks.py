9b99f0f38094173c076dd7f5b496b8cdbf3bab82
Mikael Fernandes Moreira <mikaelfernandesmoreira@gmail.com>
2023-11-23 22:42:22 -0300

    first commit

---
+++ tasks.py
@@ -0,0 +1,37 @@
+from celery import Celery
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+appCelery = Celery(
+    broker='rediss://red-clfl10vjc5ks73e83se0:aPgdk9wsk1HtjnAj00YVqENSKZ4PC5q8@oregon-redis.render.com:6379',
+    result_backend='rediss://red-clfl10vjc5ks73e83se0:aPgdk9wsk1HtjnAj00YVqENSKZ4PC5q8@oregon-redis.render.com:6379'
+)
+
+def login_server(server):
+    user = "ifrsrh@gmail.com"
+    password = "fbys pmxc kpnr utey"
+    server.starttls()
+    server.login(user, password)
+
+def send(destinatario, assunto, texto, id = None):
+    remetente = "ifrsrh@gmail.com"
+    msg = MIMEMultipart()
+    msg['From'] = remetente
+    msg['To'] = destinatario
+    msg['Subject'] = assunto
+
+    msg.attach(MIMEText(texto, 'plain', 'utf-8'))
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    login_server(server)
+    server.sendmail(remetente, destinatario, msg.as_string())
+    server.quit()
+    print("Email enviado")
+    return "Email enviado"
+
+
+@appCelery.task
+def submit():
+    send("mikaelfernandesmoreira@gmail.com", "teste", "oi")
